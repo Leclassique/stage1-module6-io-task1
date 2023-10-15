d641b73a2ff116ac165db97a0bfd25f289fe127f
Leclassique <75534147+Leclassique@users.noreply.github.com>
2023-10-15 22:55:26 +0300

    Completed task

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,24 @@
 package com.epam.mjc.io;
 
-import java.io.File;
-
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class FileReader {
-
+    private static final Logger logger = Logger.getLogger(FileReader.class.getName());
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        StringBuilder str = new StringBuilder();
+        try (BufferedReader bufferedReader = new BufferedReader(new java.io.FileReader(file))) {
+            String line = bufferedReader.readLine();
+            while (line != null) {
+                String[] strings = line.split(": ");
+                str.append(strings[1]).append(",");
+                line = bufferedReader.readLine();
+            }
+        } catch (IOException e) {
+            logger.log(Level.INFO, "Error reading file");
+        }
+        String[] params = str.toString().split(",");
+        return new Profile(params[0], params[1], params[2], params[3]);
     }
 }
